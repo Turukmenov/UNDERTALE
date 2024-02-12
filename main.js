1718b6a38896929e6016e7d9f80851a23ca1001c
Turukmenov <146660177+Turukmenov@users.noreply.github.com>
2024-02-12 23:21:13 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log('123')
+
+const yellowBtn = document.querySelector('#yellowBtn')
+const brownBtn = document.querySelector('#brownBtn')
+
+yellowBtn.addEventListener('click', () => {
+    window.open("https://www.youtube.com/watch?v=BbeeuzU5Qc8&t=1s", "_blank")
+})
+brownBtn.addEventListener('click', () => {
+    window.open("https://www.fangamer.com/collections/undertale", "_blank")
+})
+
